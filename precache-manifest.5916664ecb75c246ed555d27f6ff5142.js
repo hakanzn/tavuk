eeb02a7bd61e3eef51e54baf3fe3464d7143ebd1
hakanzn <hakanzn@gmail.com>
2022-01-12 03:07:38 +0300

    Updates

---
+++ precache-manifest.5916664ecb75c246ed555d27f6ff5142.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f8fbfe7163647c25451",
+    "url": "/tavuk/static/js/runtime~main.1f8fbfe7.js"
+  },
+  {
+    "revision": "cb3926612548b29e9f98",
+    "url": "/tavuk/static/js/main.cb392661.chunk.js"
+  },
+  {
+    "revision": "c2f1491ebd9960f84303",
+    "url": "/tavuk/static/js/1.c2f1491e.chunk.js"
+  },
+  {
+    "revision": "cb3926612548b29e9f98",
+    "url": "/tavuk/static/css/main.b017674f.chunk.css"
+  },
+  {
+    "revision": "80ed4df635979d5bb4101c4abc9feb7c",
+    "url": "/tavuk/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tavuk/precache-manifest.4b89b01a2661477b63aa012cc6881507.js"
+  "/tavuk/precache-manifest.5916664ecb75c246ed555d27f6ff5142.js"
 );
 
 workbox.clientsClaim();
