f75b15f2d3361a7fc3e482276088e6d6d1ab04da
hakanzn <hakanzn@gmail.com>
2022-01-12 12:23:17 +0300

    Updates

---
+++ precache-manifest.af975e0f2254638adda27744079797a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f8fbfe7163647c25451",
+    "url": "/tavuk/static/js/runtime~main.1f8fbfe7.js"
+  },
+  {
+    "revision": "d1f294efa127e848110a",
+    "url": "/tavuk/static/js/main.d1f294ef.chunk.js"
+  },
+  {
+    "revision": "8032f890178ca410313a",
+    "url": "/tavuk/static/js/1.8032f890.chunk.js"
+  },
+  {
+    "revision": "d1f294efa127e848110a",
+    "url": "/tavuk/static/css/main.b017674f.chunk.css"
+  },
+  {
+    "revision": "87b82aa6f081c9fef81537bcca39089c",
+    "url": "/tavuk/index.html"
+  }
+];
